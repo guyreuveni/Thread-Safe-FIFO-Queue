9ec68dd1dc35a1e5bfb6ef0d321d048198a8d495
Guy <guyreuveni98@gmail.com>
2023-06-04 12:12:20 +0300

    d

--- queue.c
+++ queue.c
@@ -6,6 +6,30 @@
 
 #include "queue.h"
 
+typedef struct q_node
+{
+    void *elem;
+    struct q_node *next;
+} q_node;
+
+typedef struct queue
+{
+    q_node *head;
+    q_node *tail;
+};
+
+typedef struct thread_node
+{
+    thrd_t tid;
+    struct thread_node *next;
+} thread_node;
+
+typedef struct waiting_threds_queue
+{
+    thread_node *head;
+    thread_node *tail;
+};
+
 atomic_size_t size = 0;
 atomic_size_t waiting_threads_num = 0;
 atomic_size_t visited_elements_num = 0;
